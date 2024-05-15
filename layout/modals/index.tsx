ad5d08e2432bab6e365fc218a2da2ae7f2e34fef
João Pedro Indicatti <joao.indicatti@pettenati.com.br>
2024-05-15 09:22:01 -0300

    FE Modal

---
+++ layout/modals/index.tsx
@@ -0,0 +1,37 @@
+
+import { motion } from "framer-motion";
+import DefaultButton from "../button/button";
+import { IconLogout2, IconSettings } from "@tabler/icons-react";
+
+
+
+interface ModalProps {
+    onClose: (response: any) => void;
+}
+
+export default function DefaultModal({onClose}: ModalProps) {
+
+
+    return (
+        <div className="fixed bottom-0 left-0 right-0  h-screen z-50 flex flex-col justify-end ">
+            <motion.div 
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            className="bg-[#000000a2] h-full" onClick={onClose}>
+            </motion.div>
+            <motion.div 
+            initial={{y: 100}}
+            animate={{y: 0}}
+            exit={{y: 100}}
+            transition={{type: "spring",stiffness: 260, damping: 35,}}
+            className="absolute bottom-0 left-0 w-full p-3 bg-gray flex flex-col items-center gap-3">
+                <div className="text-white text-xl">Tem certeza que deseja continuar?</div>
+                <div className="flex flex-col gap-3 p-3">
+                    <DefaultButton onClick={() => onClose(2)} rounded="full" wide="lg" variant="pink">Confirmar</DefaultButton>
+                    <DefaultButton onClick={() => onClose(1)} rounded="full" wide="lg" variant="darkBlue">Cancelar</DefaultButton>
+                </div>
+            </motion.div>
+        </div>
+    )
+}
